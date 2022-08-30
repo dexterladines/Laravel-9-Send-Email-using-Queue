a99067c0a4aa92776761768c8e0b5111048c7b34
Dexter Ladines <ladines.dexter@nexseed.net>
2022-08-30 14:16:22 +0800

    done with laravel9-send-email-using-queue

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>How to send mail using queue in Laravel 9</title>
+</head>
+<body>
+    <center>
+        <h2 style="padding: 23px; background: #b3deb8a1; border-bottom: 6px green solid;">
+            <a href="example.com">Visit our website: Example.com</a> 
+        </h2>
+    </center>
+
+    <p>Hi, Maam/Sir</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt porro omnis tenetur hic consectetur aliquam laboriosam quibusdam voluptates eum recusandae enim praesentium, quam tempore, ullam minus! Fugiat aperiam illo libero?</p>
+
+    <strong>Thank you Maam/Sir. :)</strong>
+
+
+</body>
+</html>
